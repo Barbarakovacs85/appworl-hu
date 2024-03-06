938b3f5ce15befa0ef6c1396c99b70438573cc31
Barbarakovacs85 <133600947+Barbarakovacs85@users.noreply.github.com>
2024-03-06 01:16:11 +0100

    add product.js

---
+++ calculator2/product.js
@@ -0,0 +1,9 @@
+export const products = [
+    {id: 1, name: 'Polisztirol díszléc ragasztása (50mm-ig)', price:250},
+    {id: 2, name: 'Polisztirol díszléc ragasztása tetőtérben vagy tagolt felületeken(50mm-ig)', price:220},
+    {id: 3, name: 'Polisztirol díszléc ragasztása tetőtérben vagy tagolt felületeken(55mm-110mm-ig)', price:280},
+    {id: 4, name: 'Polisztirol díszléc ragasztása (díszes 50mm-ig)', price:200},
+    {id: 5, name: 'Polisztirol díszléc ragasztása (díszes 55mm-110mm-ig)', price:310},
+    {id: 6, name: 'Rozetták ragasztása, festése 40cm átmérőig', price:350},
+    {id: 7, name: 'Rozetták ragasztása, festése 70cm átmérőig', price:390},
+]
