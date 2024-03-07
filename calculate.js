9ff3298c105183c8aaef560f93f06656691e2af8
Barbarakovacs85 <133600947+Barbarakovacs85@users.noreply.github.com>
2024-03-07 15:09:05 +0100

    add script minus érték tiltása,add, szöveg mező áthelyezése

--- calculate.js
+++ calculate.js
@@ -1,7 +1,15 @@
 import { products } from './product.js';
 
 function calculateTotal(price, itemId) {
-    const festekQty = parseInt(document.getElementById(`festekQty${itemId}`).value);
+    const festekQtyInput = document.getElementById(`festekQty${itemId}`);
+    let festekQty = parseInt(festekQtyInput.value);
+
+    
+    if (festekQty < 0 || isNaN(festekQty)) {
+        festekQty = 0;
+        festekQtyInput.value = 0; 
+    }
+
     const total = festekQty * price;
 
     document.getElementById(`festekTotal${itemId}`).textContent = `${total} Ft`;
@@ -11,12 +19,19 @@ function calculateTotal(price, itemId) {
 function updateTotal() {
     let total = 0;
     for (const product of products) {
-        const festekQty = parseInt(document.getElementById(`festekQty${product.id}`).value);
+        const festekQtyInput = document.getElementById(`festekQty${product.id}`);
+        let festekQty = parseInt(festekQtyInput.value);
+        
+        
+        if (festekQty < 0 || isNaN(festekQty)) {
+            festekQty = 0;
+            festekQtyInput.value = 0; 
+        }
+
         total += festekQty * product.price;
     }
 
     document.getElementById('total').textContent = `${total} Ft`;
-
 }
 
-export { calculateTotal } ;
+export { calculateTotal };
